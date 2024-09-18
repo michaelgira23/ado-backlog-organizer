5c5ecc1376a4a7f27c94161d6cc8af2ea367243e
Michael Gira <michael@gira.dev>
2024-09-18 14:00:54 -0700

    Initial Fluent 2 form

--- ado-backlog-organizer-extension/src/App.tsx
+++ ado-backlog-organizer-extension/src/App.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import { Button } from "@fluentui/react-components";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  Button,
+  Dropdown,
+  Field,
+  Input,
+  makeStyles,
+  Option,
+  Textarea,
+  Title2,
+} from "@fluentui/react-components";
+
+const workItemTypes = [
+  "Bug",
+  "Epic",
+  "Feature",
+  "Impediment",
+  "Product Backlog Item",
+  "Task",
+  "Test Case",
+  "User Story",
+];
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "8px",
+    maxWidth: "400px",
+    padding: "16px",
+  },
+  search: {
+    marginTop: "20px",
+  },
+});
 
 function App() {
+  const styles = useStyles();
+
   return (
-    <div className="App">
-      <Button>Hello World</Button>
+    <div className={styles.root}>
+      <Title2>Azure DevOps Parent Finder</Title2>
+      <Field label="Work Item Title">
+        <Input />
+      </Field>
+      <Field label="Work Item Description">
+        <Textarea placeholder="Optional" />
+      </Field>
+      <Field label="Work Item Type">
+        <Dropdown multiselect={true} placeholder="Select a work item type">
+          {workItemTypes.map((option) => (
+            <Option key={option}>{option}</Option>
+          ))}
+        </Dropdown>
+      </Field>
+      <Button appearance="primary" className={styles.search}>
+        Search
+      </Button>
     </div>
   );
 }
