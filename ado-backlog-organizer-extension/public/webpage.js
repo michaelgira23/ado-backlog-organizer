a1e49d9c89753845e9fee309b1a6c079c968c284
Michael Gira <michael@gira.dev>
2024-09-18 15:28:39 -0700

    Parse Organization and Project for Azure DevOps

--- ado-backlog-organizer-extension/public/webpage.js
+++ ado-backlog-organizer-extension/public/webpage.js
@@ -1,22 +1,47 @@
 /* eslint-disable no-undef */
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // Do something with the message!
+  console.log(request);
 
-(async () => {
-  while (true) {
-    try {
-      const response = await chrome.runtime.sendMessage({
-        organization: "organization",
-        project: "project",
-        areaPath: "path",
-      });
-      console.log(response);
-    } catch (err) {
-      // console.error(err);
-    }
+  const url = new URL(window.location.href);
+  const params = {
+    protocol: url.protocol,
+    host: url.host,
+    pathname: url.pathname,
+    search: url.search,
+    hash: url.hash,
+  };
+  console.log(params);
 
-    await sleep(1000);
+  // Currently two formats:
+  // https://dev.azure.com/msazure/One/_workitems/assignedtome/
+  // https://msazure.visualstudio.com/One/_workitems/assignedtome/
+
+  let organization = null;
+  let project = null;
+  let areaPath = null;
+
+  if (url.host.includes("dev.azure.com")) {
+    const pathParts = url.pathname.split("/");
+    organization = pathParts[1];
+    project = pathParts[2];
+  } else if (url.host.includes("visualstudio.com")) {
+    const domainParts = url.host.split(".");
+    const pathParts = url.pathname.split("/");
+    organization = domainParts[0];
+    project = pathParts[1];
   }
-})();
+
+  console.log("Parsed", {
+    organization,
+    project,
+    areaPath,
+  });
+
+  sendResponse({
+    organization,
+    project,
+    areaPath,
+  });
+});
